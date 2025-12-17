89f0e05fee1ed2f62406aeb911e93f63dcaacb17
kimza488 <kimkeen9@gmail.com>
2025-12-17 14:50:20 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+body bgcolor=#CC9900
+	table width=1238 height=530 border=0
+		tr
+			td width=23 rowspan=2&nbsp;td
+			td height=119 colspan=2 bgcolor=#006600
+				h1centerfont color=#FFFFFFพรชัยอพาร์ตเมนท์fontcenterh1
+				php include Date.php;
+			td
+			td width=43 rowspan=2&nbsp;td
+		tr
+		tr
+			td width=1007 bgcolor=#FFFFFF
+				p
+					อพาร์ทเมนต์ หรู สภาพแวดล้อมดี ปลอดภัย บริการทั้งห้องพัก รายวัน รายเดือน
+					เหมาะสำหรับ br นักศึกษา ใกล้แหล่งชุมชน และ ห้างสรรพสินค้า BigC , Lotus
+				p
+					ห้องพักทันสมัย เฟอร์นิเจอร์อย่างดี และสุขภัณฑ์ครบครัน มีสิ่งอำนวยความสะดวกมากมาย
+					อาทิ WIFI อินเทอร์เน็ตความเร็วสูงทุกห้อง มีพัดลม และเครื่องปรับอากาศให้เลือก มีทีวี
+					คอมพิวเตอร์ให้เช่า
+				p
+					เชิญชมห้องตัวอย่าง ได้ทุกวัน a href=amit.phpติดต่อสอบถามa	
+			td
+			td width=190 bgcolor=#FFFF66ul
+				lia href=home.phpหน้าแรกli
+				lia href=room.phpห้องพัก&ราคาli
+				lia herf=guest.phpสมุดเยี่ยมli
+				lia href=contact.phpติดต่อli
+				ulp
+				php include login_form.php;
+			td
+		tr
+	table
+body
